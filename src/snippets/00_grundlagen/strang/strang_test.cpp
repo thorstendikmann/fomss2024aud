db8c2079c08d18bee947723833281be3ba2d25c4
Thorsten Dikmann <115802088+thorstendikmann@users.noreply.github.com>
2024-03-19 18:19:54 +0100

    #6 - Added C++ demo snippet

---
+++ src/snippets/00_grundlagen/strang/strang_test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+#include "strang.h"
+
+int main()
+{
+    Strang z ("Hello, World!");
+
+    std::cout << "Strang: " << z << std::endl;
+
+    return 0;
+}
