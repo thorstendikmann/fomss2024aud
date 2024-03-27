c61f60974615a1a630b02f3e26f5e9ca94f15784
Max83456 <163126755+Max83456@users.noreply.github.com>
2024-03-27 17:47:53 +0000

    Bearbeitet und getestet

--- src/user/uebungsblatt_1/aufgabe4.c
+++ src/user/uebungsblatt_1/aufgabe4.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-#include <math.h>
+
 
 int main() {
-    int x, y;
-    printf("Geben Sie zwei ganze Zahlen ein: ");
-    scanf("%d %d", &x, &y);
-    printf("Multiplikationsergebnis: %d\n", x * y);
-    printf("Potenzergebnis: %.0f\n", pow(x, y));
-    return 0;
+    printf("Hello Test ");
 }
